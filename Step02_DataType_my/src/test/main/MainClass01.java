a29cef055fc980151f34323bc5b07a480d19bf81
Mr.Son <ths8976@naver.com>
2018-06-08 17:55:12 +0900

    Step02

---
+++ Step02_DataType_my/src/test/main/MainClass01.java
@@ -0,0 +1,18 @@
+package test.main;
+/*
+ * [Data type]
+ * 
+ * 1. String type
+ * 
+ * - 문자열을 나타낼때 사용하는 type 이다.
+ * 
+*/
+public class MainClass01 {
+//	run 했을때 실행순서가 시작되는 main 메소드
+	public static void main(String[] args) {
+		System.out.println("main 메소드가 시작 되었습니다.");
+		String a="김구라";
+		System.out.println("a:"+a);
+		System.out.println("main 메소드가 종료 됩니다.");
+	}
+}
