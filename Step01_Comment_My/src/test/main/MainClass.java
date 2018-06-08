5fc4cc7dce859bce3589a18d7c8aaa70b6581d94
Mr.Son <ths8976@naver.com>
2018-06-08 17:51:09 +0900

    Step01

---
+++ Step01_Comment_My/src/test/main/MainClass.java
@@ -0,0 +1,23 @@
+// 한줄 주석입니다.
+
+/*
+ * *여러줄 주석입니다.
+ * 하나
+ * 두울
+ */
+//빨간색으로 표시되는 단어는 예약어 입니다.
+
+package test.main; //package 예약어를 이용해서 패키지 표시
+
+//클래스 식별자(이름) 은 MainClass 입니다.
+public class MainClass {
+//	run 했을때 실행순서가 들어오는  main 메소드(method) 정의하기 메소드는 main
+	public static void main(String[] args) {
+		System.out.println("main 메소드가 시작됨");
+		System.out.println("하나");
+		System.out.println("두울");
+		System.out.println("세엣");
+		System.out.println("main 메소드가 종료 됩니다.");
+		System.out.println("main 메소드가 종료 됩니다.");
+	}//메소드의 끝
+}//클래스의 끝
