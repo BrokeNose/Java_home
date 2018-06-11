613756b20f2f9e81203509d6843bdf1d3bba7216
Mr.Son <ths8976@naver.com>
2018-06-11 16:55:39 +0900

    Step05, 06, 07 added

---
+++ Step07_Array_My/src/test/main/MainClass03.java
@@ -0,0 +1,10 @@
+package test.main;
+
+public class MainClass03 {
+	public static void main(String[] args) {
+		// String type 을 담을수 있는 방5개 짜리 배열 객체 생성해서 
+		//참조 값을 변수에 담기
+		//5개의 방에는 null 이 들어 있다.
+		String[] names=new String[5];
+	}
+}
